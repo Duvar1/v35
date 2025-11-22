c3a9ac5f07e52a4e4cc7227611f033f3372924e1
Duvar1 <mhmt7818@gmail.com>
2025-11-23 00:05:41 +0300

    son debug claude

--- android/app/src/main/java/com/vaktinamaz/app/MainActivity.java
+++ android/app/src/main/java/com/vaktinamaz/app/MainActivity.java
@@ -10,29 +10,3 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(StepCounterPlugin.class);
     }
 }
-```
-
----
-
-## 5. React/TypeScript - Plugin Tanımı
-**Konum:** `src/plugins/StepCounter.ts`
-
-```typescript
-import { registerPlugin } from '@capacitor/core';
-
-export interface StepCounterPlugin {
-  requestPermissions(): Promise;
-  checkPermission(): Promise;
-  startService(): Promise;
-  stopService(): Promise;
-  resetSteps(): Promise;
-  addListener(
-    eventName: 'stepUpdate',
-    listenerFunc: (data: { steps: number }) => void
-  ): Promise;
-  removeAllListeners(): Promise;
-}
-
-const StepCounter = registerPlugin('StepCounter');
-
-export default StepCounter;
