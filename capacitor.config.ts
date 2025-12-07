11b837695575ff9173d58a14cf06aeda78bb2ff2
Duvar1 <mhmt7818@gmail.com>
2025-12-07 20:36:48 +0300

    ios entegrasyonu

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,19 +4,17 @@ const config: CapacitorConfig = {
   appId: "com.vaktinamaz.app",
   appName: "Vaktinamaz",
   webDir: "dist",
-  backgroundColor: "#FFFFFF", // Buraya taşı
+  backgroundColor: "#FFFFFF",
 
   server: {
     androidScheme: "https"
   },
 
-  // ✅ SADECE TEMEL iOS AYARLARI
   ios: {
     scheme: "App",
     contentInset: "always",
     allowsLinkPreview: false,
     scrollEnabled: true,
-    // Sadece bu 4 property garantili çalışır
   },
 
   android: {
@@ -34,10 +32,8 @@ const config: CapacitorConfig = {
         "https://www.googleapis.com/auth/fitness.activity.read",
         "https://www.googleapis.com/auth/fitness.location.read",
       ],
-      serverClientId:
-        "363514939464-n7ir7squ25589sh85g45duvd5a8ttol5.apps.googleusercontent.com",
-      clientId:
-        "363514939464-n7ir7squ25589sh85g45duvd5a8ttol5.apps.googleusercontent.com",
+      serverClientId: "363514939464-n7ir7squ25589sh85g45duvd5a8ttol5.apps.googleusercontent.com",
+      clientId: "363514939464-n7ir7squ25589sh85g45duvd5a8ttol5.apps.googleusercontent.com",
       forceCodeForRefreshToken: true,
     },
 
